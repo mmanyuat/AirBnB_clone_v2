45ec89a10129fd2abd7f78afa686e587b5b8d636
mmanyuat <mmanyuat201@gmail.com>
2024-08-22 10:05:13 +0000

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,12 +5,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/', strict_slashes=False)
 
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """function to return hello"""
     return "Hello HBNB!"
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     """Ensure the script directly"""
     app.run(host="0.0.0.0", port=5000)
