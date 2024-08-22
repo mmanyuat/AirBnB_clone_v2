b2b6b99739fdff347bab19066295e41d7abb4ce0
mmanyuat <mmanyuat201@gmail.com>
2024-08-22 11:15:26 +0000

    task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""importing flask  module"""
+from flask import Flask
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """the home page"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def Hbnb():
+    """The display"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
