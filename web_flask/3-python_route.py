c15c9e3de6a0a7f2974949a74d87f7f3ba06abe6
mmanyuat <mmanyuat201@gmail.com>
2024-08-22 12:02:48 +0000

    thehbnb

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,8 +22,9 @@ def c_text(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+def python_text(text="is cool"):
     return "Python " + text.replace('_', ' ')
 
 
